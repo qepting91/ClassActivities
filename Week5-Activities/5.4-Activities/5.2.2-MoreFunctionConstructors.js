20f7e20ac3fe392e013a77f51668d9fedda59a3d
Palmetto <qepting91@gmail.com>
2023-12-08 15:40:27 -0500

    add week5 activities

---
+++ Week5-Activities/5.4-Activities/5.2.2-MoreFunctionConstructors.js
@@ -0,0 +1,8 @@
+// Create a function constructor that works as a template for an Animal
+// include properties for name, type, weight, etc.
+
+// Now create a new instance on an Animal
+
+// Add methods to the Animal prototype
+
+// Attach a method directly to the Animal instance that "overwrites" a prototype method
